9aca54bdb389f611ae6de2ae7c516d4c6cd32f4e
Juan Cruz Villena <juancruz-villena@hotmail.com>
2022-10-27 18:30:06 -0300

    Commit inicial

---
+++ suma_de_dos_nros.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int A, B, SUMA;
+    printf("ingrese A:\n");
+    scanf("%d",&A);
+    printf("igrese B:\n");
+    scanf("%d",&B);
+    SUMA = A + B;
+    printf("la suma es %d",SUMA);
+
+    return 0;
+}
+
