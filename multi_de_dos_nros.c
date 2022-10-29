6dfc63db09b2ba17e9b21b18aa70ba2c53584058
Juan Cruz Villena <juancruz-villena@hotmail.com>
2022-10-28 22:52:57 -0300

    Nuevo archivo multiplicacion de dos numeros enteros

---
+++ multi_de_dos_nros.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int A,B,MULT;
+    printf("ingrese A:\n");
+    scanf("%d",&A);
+    printf("ingrese B:\n");
+    scanf("%d",&B);
+    MULT=A*B;
+    printf("la multiplicación en %d",MULT);
+    return 0;
+}
